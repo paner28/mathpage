621a3b53384bf0a01f19eac263dcaf67df69ea44
paner28 <61655946+paner28@users.noreply.github.com>
2020-06-10 19:09:03 +0900

    Update example.js

--- js/example.js
+++ js/example.js
@@ -6,21 +6,21 @@ function cuturl(){
     return question_num;
 }
 
-console.log("6");
+console.log("");
 
 //CSVファイルを読み込む関数getCSV()の定義
-function getCSV(self){
+function getCSV(question_num){
     var req = new XMLHttpRequest(); // HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
     req.open("get", "main.csv", true); // アクセスするファイルを指定
     req.send(null); // HTTPリクエストの発行
     // レスポンスが返ってきたらconvertCSVtoArray()を呼ぶ
-    req.onload = function(self){
-	convertCSVtoArray(req.responseText,self); // 渡されるのは読み込んだCSVデータ
+    req.onload = function(question_num){
+	convertCSVtoArray(req.responseText,question_num); // 渡されるのは読み込んだCSVデータ
     }
 }
 
 // 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
-function convertCSVtoArray(str,self){ // 読み込んだCSVデータが文字列として渡される
+function convertCSVtoArray(str,question_num){ // 読み込んだCSVデータが文字列として渡される
     var result = []; // 最終的な二次元配列を入れるための配列
     var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
     console.log(result)
