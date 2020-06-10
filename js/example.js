60033f50cabf5da4c19439b590348e1c6480d374
paner28 <61655946+paner28@users.noreply.github.com>
2020-06-10 15:42:31 +0900

    Update example.js

--- js/example.js
+++ js/example.js
@@ -12,6 +12,7 @@ function getCSV(){
 // 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
 function convertCSVtoArray(str){ // 読み込んだCSVデータが文字列として渡される
     var result = []; // 最終的な二次元配列を入れるための配列
+    console.log(result)
     var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
     // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
     for(var i=0;i<tmp.length;++i){
