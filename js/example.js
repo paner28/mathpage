147f3cd8a8a1edec8f2bcb84a272387eba5fc8fe
paner28 <61655946+paner28@users.noreply.github.com>
2020-06-10 20:52:06 +0900

    Update example.js

--- js/example.js
+++ js/example.js
@@ -15,9 +15,6 @@ function getCSV(){
     req.onload = function(){
 	convertCSVtoArray(req.responseText); // 渡されるのは読み込んだCSVデータ
     }
-    var result = convertCSVtoArray(req.responseText); // 渡されるのは読み込んだCSVデータ
-    return result;
-    console.log(result);
 }
 
 // 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
@@ -29,24 +26,21 @@ function convertCSVtoArray(str){ // 読み込んだCSVデータが文字列と�
         result[i] = tmp[i].split(',');
     }
     console.log(result)
-    return result;
+
+    var q1 = document.getElementById("q1");
+    var q2 = document.getElementById("q2");
+    var q3 = document.getElementById("q3");
+
+    const child1 = document.createElement('div');
+    child1.textContent = result[question_num][2];
+    q1.appendChild(child1)
+    const child2 = document.createElement('div');
+    child2.textContent = result[question_num][3];
+    q2.appendChild(child2)
+    const child3 = document.createElement('div');
+    child3.textContent = result[question_num][4];
+    q3.appendChild(child3)
 }
 
 var question_num = cuturl();
-console.log(question_num);
 var result = getCSV(question_num);
-console.log(result);
-
-var q1 = document.getElementById("q1");
-var q2 = document.getElementById("q2");
-var q3 = document.getElementById("q3");
-
-const child1 = document.createElement('div');
-child1.textContent = result[question_num][2];
-q1.appendChild(child1)
-const child2 = document.createElement('div');
-child2.textContent = result[question_num][3];
-q2.appendChild(child2)
-const child3 = document.createElement('div');
-child3.textContent = result[question_num][4];
-q3.appendChild(child3)
