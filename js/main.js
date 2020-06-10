6f45c6ffd62e07706ff8acefc2f58eaa2ffa622f
paner28 <61655946+paner28@users.noreply.github.com>
2020-06-10 23:17:40 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,50 @@
+// urlを読みこむ関数
+function cuturl(){
+    var href = window.location.href ;
+    var question_num = Number(href.slice(45,-5));
+    return question_num;
+    console.log(question_num");
+}
+
+//CSVファイルを読み込む関数getCSV()の定義
+function getCSV(){
+    var req = new XMLHttpRequest(); // HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
+    req.open("get", "main.csv", true); // アクセスするファイルを指定
+    req.send(null); // HTTPリクエストの発行
+    // レスポンスが返ってきたらconvertCSVtoArray()を呼ぶ
+    req.onload = function(){
+	convertCSVtoArray(req.responseText); // 渡されるのは読み込んだCSVデータ
+    }
+}
+
+// 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
+function convertCSVtoArray(str){ // 読み込んだCSVデータが文字列として渡される
+    var result = []; // 最終的な二次元配列を入れるための配列
+    var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
+    // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
+    for(var i=0;i<tmp.length;++i){
+        result[i] = tmp[i].split(',');
+    }
+    console.log(result)
+
+    var q1 = document.getElementById("q1");
+    var q2 = document.getElementById("q2");
+    var q3 = document.getElementById("q3");
+
+    const child1 = document.createElement('div');
+    child1.textContent = result[question_num][2];
+    q1.appendChild(child1)
+    const child2 = document.createElement('div');
+    child2.textContent = result[question_num][3];
+    q2.appendChild(child2)
+    const child3 = document.createElement('div');
+    child3.textContent = result[question_num][4];
+    q3.appendChild(child3)
+}
+
+var question_num = cuturl();
+var result = getCSV(question_num);
+
 // ここから正誤判定
 const sendButton = document.getElementById('send');
 sendButton.addEventListener('click', showResults);
@@ -95,49 +142,3 @@ lineWidth.addEventListener("touchmove",function(){
 alpha.addEventListener("touchmove",function(){
     var alphaNum=document.getElementById("alpha").value;document.getElementById("alphaNum").innerHTML=alphaNum;
 });
-
-// urlを読みこむ関数
-function cuturl(){
-    var href = window.location.href ;
-    var question_num = Number(href.slice(45,-5));
-    return question_num;
-}
-
-//CSVファイルを読み込む関数getCSV()の定義
-function getCSV(){
-    var req = new XMLHttpRequest(); // HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
-    req.open("get", "main.csv", true); // アクセスするファイルを指定
-    req.send(null); // HTTPリクエストの発行
-    // レスポンスが返ってきたらconvertCSVtoArray()を呼ぶ
-    req.onload = function(){
-	convertCSVtoArray(req.responseText); // 渡されるのは読み込んだCSVデータ
-    }
-}
-
-// 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
-function convertCSVtoArray(str){ // 読み込んだCSVデータが文字列として渡される
-    var result = []; // 最終的な二次元配列を入れるための配列
-    var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
-    // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
-    for(var i=0;i<tmp.length;++i){
-        result[i] = tmp[i].split(',');
-    }
-    console.log(result)
-
-    var q1 = document.getElementById("q1");
-    var q2 = document.getElementById("q2");
-    var q3 = document.getElementById("q3");
-
-    const child1 = document.createElement('div');
-    child1.textContent = result[question_num][2];
-    q1.appendChild(child1)
-    const child2 = document.createElement('div');
-    child2.textContent = result[question_num][3];
-    q2.appendChild(child2)
-    const child3 = document.createElement('div');
-    child3.textContent = result[question_num][4];
-    q3.appendChild(child3)
-}
-
-var question_num = cuturl();
-var result = getCSV(question_num);
