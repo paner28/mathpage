fb7715a9a388130c92519936b565ed7c5b30f600
paner28 <61655946+paner28@users.noreply.github.com>
2020-06-11 14:28:29 +0900

    Update example.js

--- js/example.js
+++ js/example.js
@@ -46,7 +46,6 @@ window.onload=function () {
             console.log("ok");
             answer0 = result[i][4].split(" ");
             var answers = answer0.map(function(str) {return Number(str)});
-            console.log(answers);
             const child1 = document.createElement('span');
             child1.textContent = result[i][2];
             q1.appendChild(child1)
@@ -60,3 +59,5 @@ window.onload=function () {
     }
     console.log(result);
 };
+
+console.log(answers);
