aa77e1afa029daf807f52a9f5936413ad1b2c32a
paner28 <61655946+paner28@users.noreply.github.com>
2020-06-11 13:55:57 +0900

    Update example.js

--- js/example.js
+++ js/example.js
@@ -46,7 +46,8 @@ window.onload=function () {
     var result = csvToArray("main.csv");
     console.log(result);
     for (let i=1 ; i < result.length; i++){
-        if(result[i][1] == question_num){
+        if(result[i][1] = question_num){
+            console.log("ok");
             const child1 = document.createElement('span');
             child1.textContent = result[i][2];
             q1.appendChild(child1)
